9edbf320df3ed20b3220dbaa0160aaeb8b879620
Steven Amin <64430821+Steven-Amin02@users.noreply.github.com>
2026-07-25 01:05:20 +0300

    adding car record and violation header files

---
+++ Project2/CarRecord.h
@@ -0,0 +1,23 @@
+
+#include <string>
+using namespace std;
+
+class CarRecord
+{
+private:
+    string plateNumber;
+    string date;
+    string carType;
+    int speed;
+    bool seatbeltFastened;
+
+public:
+    CarRecord(string plate, string d, string type, int s, bool seatbelt);
+
+    string getPlateNumber() const;
+    string getDate() const;
+    string getCarType() const;
+    int getSpeed() const;
+    bool isSeatbeltFastened() const;
+};
+
---
+++ Project2/Violation.h
@@ -0,0 +1,16 @@
+
+#include <string>
+using namespace std;
+
+class Violation
+{
+private:
+    string description;
+    int fineAmount;
+
+public:
+    Violation(string desc, int fine);
+
+    string getDescription() const;
+    int getFineAmount() const;
+};
